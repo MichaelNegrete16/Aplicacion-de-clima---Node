0025b07c07b0bc59e8cb219345fcff509efd78df
Maicol <michael.negrete16@gmail.com>
2022-07-16 08:27:33 -0500

    Modelo para poder controlar la aplicacion

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 // Helpers
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer")
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+// Models
+const Busquedas = require("./models/busquedas");
 
 const main = async() => {
 
+    const busqueda = new Busquedas()
     let opt
 
     do {
         
         // Imprimir el menu
         opt = await inquirerMenu()
+
+        switch (opt) {
+            case 1:
+                // Mostrar mensaje
+                const lugar = await leerInput('Ciudad: ')
+                console.log(lugar)
+                // Buscar la ciudad o lugar
+
+                // Seleccionar el lugar
+
+                // Datos del clima
+
+                // Mostrar resultados
+                console.log('\nInformacion de la ciudad\n'.green)
+                console.log('Ciudad:',)
+                console.log('Latitud:',)
+                console.log('Longitud:',)
+                console.log('Temperatura:',)
+                console.log('Temperatura Minima:',)
+                console.log('Temperatura Maxima:',)
+
+                break;
+        
+            default:
+                break;
+        }
 
         await pausa()
 
---
+++ models/busquedas.js
@@ -0,0 +1,19 @@
+
+class Busquedas {
+    historial = []
+
+    constructor(){
+        // Todo: leer Db si existe
+    }
+
+    // Buscar una ciudad
+    async ciudad( lugar =''){
+        // Peticion http
+        console.log(lugar)
+
+        return [] //Retorna los lugares que coincidan con el lugar que escribe la persona
+    }
+
+}
+
+module.exports = Busquedas
