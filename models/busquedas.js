b0265928b0e61bebc6c770b658db4ddae9f57dfd
Maicol <michael.negrete16@gmail.com>
2022-07-20 14:24:22 -0500

    Crear instancia con axios

--- models/busquedas.js
+++ models/busquedas.js
@@ -8,14 +8,26 @@ class Busquedas {
         // Todo: leer Db si existe
     }
 
+    get paramsMapbox(){
+        return {
+            'access_token':'pk.eyJ1IjoiamFja3NvbjAxIiwiYSI6ImNsNW8zOWxsdzA4M2gzY21vZ3ZrMW53Zm8ifQ.qEkMkuZf11kQ8F5Q-ohWyg',
+            'limit': 5,
+            'languaje': 'es'
+        }
+    }
+
     // Buscar una ciudad
     async ciudad( lugar =''){
        
         try {
              // Peticion http
-            // console.log('Ciudad: ',lugar)
-            const respuesta = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/carta.json?country=co&language=es&access_token=pk.eyJ1IjoiamFja3NvbjAxIiwiYSI6ImNsNW8zOWxsdzA4M2gzY21vZ3ZrMW53Zm8ifQ.qEkMkuZf11kQ8F5Q-ohWyg')
-            console.log(respuesta.data)
+            const intance = axios.create({
+                baseURL:`https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            })
+            // Hacer peticion
+            const resp = await intance.get()
+            console.log(resp.data)
 
             return []
 
